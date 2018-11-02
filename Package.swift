b5e9be20f7af58d035c7a6cf945b3ee8ed5c7e2e
cbepxbeo <cbepxbeo@mail.ru>
2018-11-02 13:01:08 +0300

    fix

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
+// swift-tools-version:4.2
+
 import PackageDescription
 
 let package = Package(
-    name: "SwiftUtilities"
+    name: "SwiftUtilities",
+    products: [
+        .library(
+            name: "SwiftUtilities",
+            targets: ["SwiftUtilities"])
+    ],
+    targets: [
+        .target(
+            name: "SwiftUtilities",
+            path: "Source")
+    ],
+    swiftLanguageVersions: [3, 4]
 )
