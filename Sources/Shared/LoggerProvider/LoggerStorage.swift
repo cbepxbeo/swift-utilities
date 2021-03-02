c44f3df85c243dea3826bee2d4c37c4b532dee0a
cbepxbeo <cbepxbeo@mail.ru>
2021-03-02 13:44:48 +0300

    work

---
+++ Sources/Shared/LoggerProvider/LoggerStorage.swift
@@ -0,0 +1,12 @@
+/*
+
+Project: SwiftUtilities
+File: LoggerStorage.swift
+Created by: Egor Boyko
+Date: 02.03.2021
+
+*/
+
+import OSLog
+
+internal var loggerStorage: [String: Logger] = [:]
---
+++ Tests/SwiftUtilitiesTests/Shared/SwiftUtilitiesTests.swift
@@ -0,0 +1,13 @@
+/*
+ 
+ Project: SwiftUtilities
+ File: SwiftUtilitiesTests.swift
+ Created by: Egor Boyko
+ Date: 18.05.2023
+ 
+*/
+
+import XCTest
+@testable import SwiftUtilities
+
+final class SwiftUtilitiesTests: XCTestCase {}
