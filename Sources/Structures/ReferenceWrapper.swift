47fcb370422d821c4795ac1d169a6d5a31a482e0
cbepxbeo <cbepxbeo@mail.ru>
2023-06-03 02:57:58 +0300

    add ReferenceWrapper

---
+++ Sources/Structures/ReferenceWrapper.swift
@@ -0,0 +1,18 @@
+/*
+
+Project: SwiftUtilities
+File: ReferenceWrapper.swift
+Created by: Egor Boyko
+Date: 03.06.2023
+
+Status: #Complete | #Not decorated
+
+*/
+
+public final class ReferenceWrapper<T> {
+    public init(_ value: T) {
+        self.value = value
+    }
+    
+    public let value: T
+}
