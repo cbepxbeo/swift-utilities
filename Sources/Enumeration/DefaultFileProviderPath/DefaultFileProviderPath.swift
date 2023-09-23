9626d24a61881dfdb302ffd7c60df0dadae5f05e
Egor Olegovich <cbepxbeo@mail.ru>
2023-09-23 18:07:40 +0300

    Create DefaultFileProviderPath.swift

---
+++ Sources/Enumeration/DefaultFileProviderPath/DefaultFileProviderPath.swift
@@ -0,0 +1,16 @@
+/*
+ 
+ Project: SwiftUtilities
+ File: DefaultFileProviderPath.swift
+ Created by: Egor Boyko
+ Date: 23.09.2023
+ 
+ Status: #Complete | #Not decorated
+ 
+ */
+
+public enum DefaultFileProviderPath: String, FileProviderPath {
+    case images
+    case video
+    case other
+}
