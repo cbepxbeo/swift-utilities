cd6f3cdfdbc892f1086e6c48910effa2e6e30c24
Egor Olegovich <cbepxbeo@mail.ru>
2023-09-23 13:59:46 +0300

    Update EnumConvertable

---
+++ Sources/Protocols/EnumConvertable/EnumConvertable.swift
@@ -0,0 +1,12 @@
+/*
+ 
+ Project: SwiftUtilities
+ File: EnumConvertable.swift
+ Created by: Egor Boyko
+ Date: 03.01.2022
+ 
+ Status: #Complete | #Not decorated
+ 
+ */
+
+public protocol EnumConvertable: CaseIterable {}
