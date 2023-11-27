8cb757c0a00b084ae7ad4fa2bac43e095962eb25
cbepxbeo <cbepxbeo@mail.ru>
2023-11-27 21:02:04 +0300

    Update Logging.swift

--- Sources/Protocols/Logging/Logging.swift
+++ Sources/Protocols/Logging/Logging.swift
@@ -12,6 +12,9 @@
 import OSLog
 
 public protocol Logging {
+    static var logger: Logger { get }
+    static var loggerCategory: String? { get }
     var logger: Logger { get }
+    @available(*,deprecated, message: "Use static var loggerCategory")
     var loggerCategory: String? { get }
 }
